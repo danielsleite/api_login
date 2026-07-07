45488ed042bb26a577e920cf70956f4e9274e273
Daniel Leite <daniels.leite@hotmail.com>
2026-07-07 11:26:07 -0300

    adicionadas coluna das ultimas senhas,  criada funcao para verificar se a senha é forte. Removido o Id como chave primaria e colocada o login. Colocada a coluna de nivel de linha

--- model/ad.py
+++ model/ad.py
@@ -1,22 +1,36 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from model import Base
+import re
 
 """
     Classe Login
     A classe cria uma representação genérica de um Login, com os campos login, senha e email
     Cria a tabela de login Login para retenção desses dados no banco.
 """
-
-
+ 
 class Usuario(Base):
     __tablename__ = "Login"
 
-    id = Column("id_login", Integer, primary_key=True)
-    login = Column("login", String(15), unique=True, nullable=False)
+    login = Column("login", String(15), unique=True, nullable=False, primary_key=True)
     senha = Column("senha", String(15), unique=False, nullable=False)
     email = Column("email", String(15), unique=True, nullable=False)
     alterar_senha = Column("altera_senha", Boolean, nullable=False)
     cadastrado_por = Column("cadastrado_por", String(15))
+    nivel = Column("nivel", Integer, nullable=False, default=1)  # nivel de acesso do usuario, 1 = usuario nivel, 2 = usuario nivel 2, 3 = usuario nivel 3, 4 = usuario nivel 4, 5 = usuario nivel 5, 6 = usuario nivel 6, 7 = usuario nivel 7, 8 = usuario nivel 8, 9 = usuario nivel 9, 10 = usuario nivel 10
+    senhaOld1 = Column("senhaOld1", String(15), unique=False, nullable=True)
+    senhaOld2 = Column("senhaOld2", String(15), unique=False, nullable=True)
+    senhaOld3 = Column("senhaOld3", String(15), unique=False, nullable=True) 
+
+    def is_strong_password(self, password: str) -> bool:
+        if len(password) < 8:
+            return False
+
+        has_upper = re.search(r"[A-Z]", password) is not None
+        has_lower = re.search(r"[a-z]", password) is not None
+        has_number = re.search(r"\d", password) is not None
+        has_special = re.search(r"[!@#$%^&*(),.?\":{}|<>]", password) is not None
+
+        return has_upper and has_lower and has_number and has_special
 
     def __init__(
         self,
@@ -25,6 +39,8 @@ class Usuario(Base):
         email: str,
         cadastrado_por: str,
         alterar_senha: bool = True,
+        nivel: int = 1, # nivel de acesso do usuario, 1 = usuario nivel, 2 = usuario nivel 2, 3 = usuario nivel 3, 4 = usuario nivel 4, 5 = usuario nivel 5, 6 = usuario nivel 6, 7 = usuario nivel 7, 8 = usuario nivel 8, 9 = usuario nivel 9, 10 = usuario nivel 10
+        
     ) -> None:
         super().__init__()
         self.login = login
@@ -32,3 +48,4 @@ class Usuario(Base):
         self.email = email
         self.cadastrado_por = cadastrado_por
         self.alterar_senha = alterar_senha
+        self.nivel = nivel
